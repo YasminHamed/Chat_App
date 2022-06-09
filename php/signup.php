791890f1fd309aad482fff5993d5bfbd36265541
YasminHamed <98949809+YasminHamed@users.noreply.github.com>
2022-06-10 00:30:25 +0300

    Update signup.php

--- php/signup.php
+++ php/signup.php
@@ -13,7 +13,7 @@
             }else{
                 if(isset($_FILES['image'])){
                     $img_name = $_FILES['image']['name'];
-                    // $img_type = $_FILES['image']['type'];
+                    $img_type = $_FILES['image']['type'];
                     $tmp_name = $_FILES['image']['tmp_name'];
                     
                     $img_explode = explode('.',$img_name);
@@ -30,7 +30,7 @@
                                 $status = "Active now";
                                 $encrypt_pass = md5($password);
                                 $insert_query = mysqli_query($conn, "INSERT INTO users (unique_id, fname, lname, email, password, img, status)
-                                                            VALUES ({$ran_id}, '{$fname}','{$lname}', '{$email}', '{$encrypt_pass}', '{$new_img_name}', '{$status}')");
+                                VALUES ({$ran_id}, '{$fname}','{$lname}', '{$email}', '{$encrypt_pass}', '{$new_img_name}', '{$status}')");
                                 if($insert_query){
                                     $select_sql2 = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'");
                                     if(mysqli_num_rows($select_sql2) > 0){
